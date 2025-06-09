f639b30d747785dfcc76b04928aa19fc0e0176a7
mamaans <55975961+mamaans@users.noreply.github.com>
2025-06-10 01:17:42 +0300

    Add .next/static/chunks/src_components_staff-card_tsx_fe111ff7._.js 1749507462299

---
+++ .next/static/chunks/src_components_staff-card_tsx_fe111ff7._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/staff-card.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/src_components_staff-card_tsx_d670915c._.js",
+  "static/chunks/src_components_staff-card_tsx_ee0c2e14._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/staff-card.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
