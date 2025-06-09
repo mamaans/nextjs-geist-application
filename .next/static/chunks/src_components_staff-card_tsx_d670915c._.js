736ec1002c98e57a0ea9fbc16e803c41da5e4f19
mamaans <55975961+mamaans@users.noreply.github.com>
2025-06-10 01:17:36 +0300

    Add .next/static/chunks/src_components_staff-card_tsx_d670915c._.js 1749507455820

---
+++ .next/static/chunks/src_components_staff-card_tsx_d670915c._.js
@@ -0,0 +1,18 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/staff-card.tsx [app-client] (ecmascript)": (function(__turbopack_context__) {
+
+var { g: global, __dirname, k: __turbopack_refresh__, m: module, e: exports } = __turbopack_context__;
+{
+const e = new Error(`Could not parse module '[project]/src/components/staff-card.tsx'
+
+Unexpected token `Card`. Expected jsx identifier`);
+e.code = 'MODULE_UNPARSEABLE';
+throw e;}}),
+"[project]/src/components/staff-card.tsx [app-client] (ecmascript, next/dynamic entry)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.n(__turbopack_context__.i("[project]/src/components/staff-card.tsx [app-client] (ecmascript)"));
+}}),
+}]);
