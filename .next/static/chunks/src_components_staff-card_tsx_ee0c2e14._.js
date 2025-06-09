1611b35f561fc02f921f8d489506b8355cc506ef
mamaans <55975961+mamaans@users.noreply.github.com>
2025-06-10 01:17:32 +0300

    Add .next/static/chunks/src_components_staff-card_tsx_ee0c2e14._.js 1749507451681

---
+++ .next/static/chunks/src_components_staff-card_tsx_ee0c2e14._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_staff-card_tsx_d670915c._.js"
+],
+    source: "dynamic"
+});
